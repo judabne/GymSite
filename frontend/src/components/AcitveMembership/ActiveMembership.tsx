818a47a9876a75279598f64d78177b432a6e0cd3
judabne <jud_abinehme@hotmail.com>
2021-11-27 12:52:13 +0200

    passing classes as props to ActiveMembership.tsx

--- frontend/src/components/AcitveMembership/ActiveMembership.tsx
+++ frontend/src/components/AcitveMembership/ActiveMembership.tsx
@@ -1,28 +1,20 @@
 import React from "react";
 // @material-ui/core components
-import { withStyles } from "@material-ui/core/styles";
-import { makeStyles, Theme, createStyles, Grid } from "@material-ui/core";
-import { BaseCSSProperties } from '@material-ui/core/styles/withStyles';
+import { Grid } from "@material-ui/core";
 // core components
-import GridContainer from "components/Grid/GridContainer.js";
-import GridItem from "components/Grid/GridItem.js";
-import styles from "assets/jss/material-kit-react/views/profilePage.js";
 import Divider from "@material-ui/core/Divider";
 
-console.log(styles)
-const useStyles = makeStyles({styles});
-console.log(useStyles)
+interface Keyable {
+    [key: string]: string
+}
 
 interface Props {
-    planType: any,
-    expiry: any,
-    classes?: object
+    planType: string,
+    expiry: string,
+    classes: Keyable
 };
 
-function ActiveMembership(props: Props) {
-    const classes = useStyles();
-    console.log(classes)
-
+const ActiveMembership: React.FC<Props> = (props) => {
     var todayDate: any = new Date();
     let remainingDays = Math.ceil((Date.parse(props.expiry) - todayDate) / (1000 * 3600 * 24))
 
@@ -33,7 +25,8 @@ function ActiveMembership(props: Props) {
                     <h5>{props.planType} Membership</h5>
                 </Grid>
                 {/* the line below should use className={classes.textRight} instead of this */}
-                <Grid item xs={6} sm={6} md={6} style={{textAlign: "right"}}>
+                <Grid item xs={6} sm={6} md={6} className={props.classes.textRight}>
+
                     <h5>{remainingDays} day{remainingDays !== 1 && 's'} remaining</h5>
                 </Grid>
             </Grid>
